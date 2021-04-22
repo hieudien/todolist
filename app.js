db89e32e7c8672a3e8f2758cebd1e3b5e3171f69
hieudien <hieudthd97@gmail.com>
2021-04-22 15:29:37 +0700

    remove img on remove item

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require('cors');
-require("dotenv").config();
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const path = require("path")
+const cors = require('cors')
+require("dotenv").config()
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error.message));
-db.on("open", () => console.log("Connected to DB"));
+})
+const db = mongoose.connection
+db.on("error", (error) => console.error(error.message))
+db.on("open", () => console.log("Connected to DB"))
 
 // allow cors
-app.use(cors());
-app.options('*', cors());
+app.use(cors())
+app.options('*', cors())
 
 // allow app use json
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}))
 
 // make uploads/images folder be static to client
 app.use('/images', express.static(process.env.UPLOAD_IMAGE_PATH))
 
 // set Item router
-const itemRouter = require("./routes/Item");
-app.use("/item", itemRouter);
+const itemRouter = require("./routes/Item")
+app.use("/item", itemRouter)
 
 // set home page
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname + "/views/index.html"))
-);
+)
 
 // Listening to port
-app.listen(process.env.PORT, () => console.log("Server running at http://localhost:" + process.env.PORT));
+app.listen(process.env.PORT, () => console.log("Server running at http://localhost:" + process.env.PORT))
--- routes/Item.js
+++ routes/Item.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 const multer = require("multer")
-const Item = require("../models/Item");
+const Item = require("../models/Item")
+const fs = require('fs')
 
 // setup multer storeage
 const storage = multer.diskStorage({
@@ -18,78 +19,80 @@ const upload = multer({ storage: storage })
 // Get all Item
 router.get("/", async (req, res) => {
   try {
-    let items;
+    let items
     // filter by isDone
     if (req.query.status === "done") {
       // find items is done
-      items = await Item.find({ isDone: true });
+      items = await Item.find({ isDone: true })
     } else if (req.query.status === "notyet") {
       // find items is not done
-      items = await Item.find({ isDone: false });
+      items = await Item.find({ isDone: false })
     } else {
       // find all items
-      items = await Item.find();
+      items = await Item.find()
     }
-    res.status(200).json(items);
+    res.status(200).json(items)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-});
+})
 
 // Create Item
 router.post("/", upload.single("image"), async (req, res) => {
-  console.log(req.file);
   const item = new Item({
     name: req.body.name,
     image: req.file.filename
-  });
+  })
   try {
-    const newItem = await item.save();
-    res.status(201).json(newItem);
+    const newItem = await item.save()
+    res.status(201).json(newItem)
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message })
   }
-});
+})
 
 // Update Item
 router.patch("/:id", getItem, async (req, res) => {
   if (req.body.name) {
-    res.item.name = req.body.name;
+    res.item.name = req.body.name
   }
   if (req.body.isDone !== null) {
-    res.item.isDone = req.body.isDone;
+    res.item.isDone = req.body.isDone
   }
   try {
-    const updatedItem = await res.item.save();
-    res.status(200).json(updatedItem);
+    const updatedItem = await res.item.save()
+    res.status(200).json(updatedItem)
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message })
   }
-});
+})
 
 // Delete Item
 router.delete("/:id", getItem, async (req, res) => {
   try {
-    await res.item.remove();
-    res.json({ message: "Item deleted" });
+    // remove item
+    await res.item.remove()
+    // remove image
+    fs.unlink(process.env.UPLOAD_IMAGE_PATH + res.item.image, (err) => err)
+    res.json({ message: "Item deleted" })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-});
+})
 
 // middleware to get Item
 async function getItem(req, res, next) {
-  let item;
+  let item
   try {
-    item = await Item.findById(req.params.id);
+    item = await Item.findById(req.params.id)
     if (!item) {
-      return res.status(404).json({ message: "Item not found" });
+      return res.status(404).json({ message: "Item not found" })
     }
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message })
   }
 
-  res.item = item;
-  next();
+  res.item = item
+  next()
 }
-module.exports = router;
+module.exports = router
